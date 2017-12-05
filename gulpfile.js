7993941d5df4c17a1f596132d1284ce4c1eb9d4b
Jun Kato <i@junkato.jp>
2017-12-05 10:48:14 +0900

    Add a less file

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,46 @@
 var gulp = require('gulp');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-csso');
+var browserSync = require('browser-sync');
 
 gulp.task('html', function(){
-  return gulp.src('src/*.pug')
+  return gulp.src(['src/**/*.pug', '!src/**/_*.pug'])
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
     .pipe(pug())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('css', function(){
-  return gulp.src('src/stylesheets/*.less')
+  return gulp.src('src/stylesheets/**/*.less')
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
     .pipe(less())
+    .pipe(autoprefixer())
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/stylesheets'))
 });
 
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: {
+      baseDir: 'dist/'
+    }
+  });
+  gulp.watch('dist/**/*.html', ['reload']);
+  gulp.watch('dist/stylesheets/**/*.css', ['reload']);
+});
+
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*.pug', '!src/**/_*.pug'], ['html']);
+  gulp.watch('src/stylesheets/**/*.less', ['css']);
+});
+
 gulp.task('default', [ 'html', 'css' ]);
+gulp.task('sync', [ 'default', 'browser-sync', 'watch' ]);
