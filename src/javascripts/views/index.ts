1feaaa24a303df384eeea6f4dddaf74c8ab33a21
Jun Kato <i@junkato.jp>
2018-01-07 17:01:28 +0900

    Enable lazy loading of history entries

--- src/javascripts/views/index.ts
+++ src/javascripts/views/index.ts
@@ -1,4 +1,6 @@
 
+const lang: 'en'|'ja' = (<any>self)["lang"];
+
 $('a.sns').on('click touch', (ev) => {
   setTimeout(() => $('#sns').transition('jiggle'), 700);
   return true;
@@ -9,29 +11,38 @@ import(/* webpackChunkName: "library" */ './library').then(TestLib => {
   console.log('dynamically loaded library: 1+2=', testLib.test(1, 2));
 });
 
-import(/* webpackChunkName: "histories" */ '../histories').then(histories => {
-  const lang: 'en'|'ja' = (<any>self)["lang"];
-  const entries = histories.default[lang];
-  const num = 5;
-  var cursor = num;
-  $('a.more')
-    .removeClass('disabled')
-    .on('click touch', () => {
-      const es = entries.slice(cursor, cursor + num)
-        , $historyList = $('#history .ui.segment .ui.list')
-      var $added = null;
-      for (const e of es) {
-        const $item = $('<div class="item"><i class="right triangle icon"></i><div class="content"><div class="header"></div><div class="description"></div></div></div>');
-        $item.find('.header').html(e.text);
-        $item.find('.description').text(e.getDateString(lang));
-        $historyList.append($item);
-        if (!$added) $added = $item;
-        else $added = $added.add($item);
-      }
-      cursor += num;
-      if (cursor > entries.length) {
-        $('a.more').addClass('disabled');
-      }
-      if ($added) $added.transition('jiggle');
+const num = 5;
+const $moreButton = $('a.more');
+var cursor = num, entries: any[];
+$moreButton
+  .removeClass('disabled')
+  .on('click touch', () => {
+    if (entries) {
+      return handleEntries(entries);
+    }
+    $moreButton.addClass('loading disabled');
+    import(/* webpackChunkName: "histories" */ '../histories').then((histories) => {
+      $moreButton.removeClass('loading disabled');
+      entries = histories.default[lang];
+      handleEntries(entries);
     });
 });
+
+function handleEntries(entries: any[]) {
+  const es = entries.slice(cursor, cursor + num)
+    , $historyList = $('#history .ui.segment .ui.list')
+  var $added = null;
+  for (const e of es) {
+    const $item = $('<div class="item"><i class="right triangle icon"></i><div class="content"><div class="header"></div><div class="description"></div></div></div>');
+    $item.find('.header').html(e.text);
+    $item.find('.description').text(e.getDateString(lang));
+    $historyList.append($item);
+    if (!$added) $added = $item;
+    else $added = $added.add($item);
+  }
+  cursor += num;
+  if (cursor > entries.length) {
+    $('a.more').addClass('disabled');
+  }
+  if ($added) $added.transition('jiggle');
+}
