ab8d655e1df06747982b024675d3f3774e5d3202
Jun Kato <i@junkato.jp>
2018-02-14 01:29:16 +0900

    Add image modal in the design portfolio

---
+++ src/javascripts/views/design.ts
@@ -0,0 +1,14 @@
+
+import(/* webpackChunkName: "library" */ './library').then(Library => {
+  $('a.ui.for-modal.image').click(function(){
+    const $a = $(this)
+      , $title = $a.parents('.ui.segment').find('h3.ui.header')
+      , $img = $a.find('img')
+      , $modal = $('.ui.modal')
+      , $modalHeader = $modal.find('.header')
+      , $modalImg = $modal.find('img');
+    $modalHeader.html($title.html());
+    $modalImg.attr('src', <string>$img.attr('src'));
+    $modal.modal('show');
+  });
+});
