142f24ee2a58115f422855c0a93d814e392d4df2
Jun Kato <i@junkato.jp>
2022-11-20 17:49:50 +0900

    Update redirections cf. https://github.com/arcatdmz/blog/commit/fc38a2c7f020c0032d776133dbea03a3ba4daa50

--- scripts/add-redirections.mjs
+++ scripts/add-redirections.mjs
@@ -11,14 +11,35 @@ mkdirp("dist/ja/blog/").then(() => {
 
 fetch("https://blog.junkato.jp/redirections.json").then(async (res) => {
   const redirections = await res.json();
-  return Promise.all(
-    redirections.map(async (redirection) => {
+
+  async function traverseCategories(cat, basePath) {
+    return Object.entries(cat, ([key, value]) => {
+      const dirPath = `${basePath}${key}`;
+      const promise = (async function () {
+        await mkdirp(dirPath);
+        fs.writeFileSync(
+          `${dirPath}/index.html`,
+          `<html><head><meta http-equiv="refresh" content="0;URL='https://blog.junkato.jp/ja/tags/${key}/'" /></head></html>`
+        );
+      })();
+      if (typeof value === "boolean") {
+        return promise;
+      }
+      const promises = traverseCategories(value, `${dirPath}/`);
+      promises.unshift(promise);
+      return promises;
+    });
+  }
+
+  return Promise.all([
+    ...redirections.articles.map(async (redirection) => {
       const dirPath = `dist/ja/blog/${redirection.old}`;
       await mkdirp(dirPath);
       fs.writeFileSync(
         `${dirPath}/index.html`,
         `<html><head><meta http-equiv="refresh" content="0;URL='https://blog.junkato.jp/ja/posts/${redirection.new}/'" /></head></html>`
       );
-    })
-  );
+    }),
+    ...traverseCategories(redirections.categories, "dist/ja/blog/category/")
+  ]);
 });
